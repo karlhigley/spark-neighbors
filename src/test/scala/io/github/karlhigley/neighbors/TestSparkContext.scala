ad96f17a0629d4c903746873a78b54a13c8f80c6
Karl Higley <kmhigley@gmail.com>
2016-01-03 12:59:59 -0500

    Add TestSparkContext

---
+++ src/test/scala/io/github/karlhigley/neighbors/TestSparkContext.scala
@@ -0,0 +1,25 @@
+package io.github.karlhigley.neighbors
+
+import org.scalatest.{ BeforeAndAfterAll, Suite }
+
+import org.apache.spark.{ SparkConf, SparkContext }
+
+trait TestSparkContext extends BeforeAndAfterAll { self: Suite =>
+  @transient var sc: SparkContext = _
+
+  override def beforeAll() {
+    super.beforeAll()
+    val conf = new SparkConf()
+      .setMaster("local[2]")
+      .setAppName("LshUnitTest")
+    sc = new SparkContext(conf)
+  }
+
+  override def afterAll() {
+    if (sc != null) {
+      sc.stop()
+    }
+    sc = null
+    super.afterAll()
+  }
+}
